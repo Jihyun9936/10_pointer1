b19aa4ca92a9d65d515bd717632af922ca5b4b20
Jihyun9936 <55350908+Jihyun9936@users.noreply.github.com>
2019-11-07 15:41:06 +0900

    Lab04

--- main.c
+++ main.c
@@ -3,7 +3,7 @@
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
-/*Lab02*/
+/*Lab02
 int main(int argc, char *argv[]) {
 	int i = 10;
 	char c='a';
@@ -16,7 +16,7 @@ int main(int argc, char *argv[]) {
 	printf("c : %p\n%p (size : %i)\n", cptr, &c, sizeof(cptr));
 	printf("iptr : %p, %i\n", iptr2, *iptr2);
 	return 0;
-}
+}*/
 
 /*Lab03
 void main(){
@@ -27,4 +27,27 @@ void main(){
 	printf("%i, %i, %i\n", i, *pi, *pc);
 }*/
 
-/*L*/
+/*Lab04*/
+void swap1 (int x, int y){
+	int temp;
+	temp = x;
+	x = y;
+	y = temp;
+}
+
+void swap2 (int *x, int *y){
+	int temp;
+	temp = *x;
+	*x = *y;
+	*y = temp;
+}
+main(){
+	int a = 3;
+	int b = 5;
+	
+	swap1(a, b);
+	printf("a : %i, b : %i\n", a, b);
+	
+	swap2(&a, &b);
+	printf("a : %i, b : %i\n", a, b);
+}
